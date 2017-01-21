7c40223f19fd6e945ce19be43bd938ff41eded2b
Dušan Miletić <d@miletich.cc>
2017-01-21 17:45:56 +0100

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import expect from 'expect';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+describe('App', () => {
+  it('should properly run tests', () => {
+    expect(1).toBe(1);
+  });
 });
