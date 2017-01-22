2fd522b0098d1702c3164b9c4b6ff5a70b05f606
Dušan Miletić <d@miletich.cc>
2017-01-22 11:31:16 +0100

    Make Countdown component count down

--- src/Countdown.js
+++ src/Countdown.js
@@ -6,14 +6,36 @@ class Countdown extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
+      countdownStatus: 'stopped',
     };
     this.handleSetCountdown = this.handleSetCountdown.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  }
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      const newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+    }, 1000);
   }
 
   handleSetCountdown(seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'started'
     });
   }
 
